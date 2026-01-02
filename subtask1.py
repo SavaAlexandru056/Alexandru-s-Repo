b1cf14b179b7cb411668f09ca67d80bf62191c2d
SavaAlexandru056 <S2510769022@fhooe.at>
2026-01-02 22:28:29 +0200

    Create subtask1.py

---
+++ subtask1.py
@@ -0,0 +1,15 @@
+# Subtask 8.1
+
+n = int(input("Enter n: "))
+
+q = 0
+r = 0
+k = 1
+
+while r + k <= n:
+    r = r + k
+    q = q + 1
+    k = k + 2
+
+print("q =", q)
+# it looks like I learned how to use git today
